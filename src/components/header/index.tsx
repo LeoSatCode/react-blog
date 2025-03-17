6517b891a7e4c19b38c7173df679ef9de6361f16
LeoSatCode <your-email@example.com>
2025-03-17 13:44:38 -0300

    social media links added

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -12,10 +12,18 @@ const Header = () => {
           <HeaderImg src={logo.src} alt="logo" />
           </Link>
           <Row>
+          <Link href={"https://www.linkedin.com/in/leonardo-saturnino-564502336"} target="_blank">
             <Menu>Linkedin</Menu>
+          </Link>
+          <Link href={"https://www.workana.com/freelancer/bd960c85ca3d39a228f856e83cca2c29"} target="_blank">
             <Menu>Workana</Menu>
+          </Link>
+          <Link href={"https://github.com/LeoSatCode"} target="_blank">
             <Menu>Github</Menu>
+          </Link>
+          <Link href={"https://www.youtube.com/@guildadosladinos"} target="_blank">
             <Menu>Youtube</Menu>
+          </Link>
           </Row>
         </Container>
       </Wrapper>
